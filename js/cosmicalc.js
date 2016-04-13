ac1908adff9a8f8a4d046d873001b08528f8ef0b
blead <blead010@gmail.com>
2016-04-13 23:09:23 +0700

    change url

--- js/cosmicalc.js
+++ js/cosmicalc.js
@@ -27,7 +27,7 @@
 var VERSION = "3.4.3",
 	LAST_MODIFIED = "2015.01.26",
 
-	CODEW_URL = "http://cosmiccalc.github.io/",
+	CODEW_URL = "http://cosmiccalc.appspot.com/",
 	
 	MAXLEVEL_DEF = 10;
 	ADDLEVEL_DEF = 0;
